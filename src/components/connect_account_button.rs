30d84007771b3cf748e91feba1229b79ea987127
legendaryphoenixicarus <legendary.phoenix.icarus@gmail.com>
2022-06-12 10:11:04 +0300

    add: on function that can handle event

--- src/components/connect_account_button.rs
+++ src/components/connect_account_button.rs
@@ -18,19 +18,42 @@ pub enum Msg {
 
 #[function_component(ConnectButtonComponent)]
 pub fn create() -> Html {
+    wasm_logger::init(wasm_logger::Config::default());
+
+    let greeting = use_state(|| "No one has greeted me yet!".to_string());
+
     let account = use_context::<Rc<AccountState>>().expect("No context found.");
 
+    let provider = account.clone();
+
+    {
+        let greeting = greeting.clone();
+        spawn_local(async move {
+
+            provider.on("connected".to_string(), move |chain_id| {
+                greeting.set(chain_id.to_string());
+            }).await;    
+        });
+    }
+
+    // let aa = greeting.to_string();
     let connect_wallet = Callback::from(move |_| {
+        
         let acc = account.clone();
+
         spawn_local(async move {
             acc.connect().await;
         });
     });
 
+    
+
     html! {
         <div>
             <button onclick={connect_wallet}>{"Connect"}</button>
-            // { format!("{:?}", account) }
+            <div>
+            <span>{"Chain Id: "}</span>{ (*greeting).to_string() }
+            </div>
         </div>
     }
 }
--- src/components/ethereum_provider.rs
+++ src/components/ethereum_provider.rs
@@ -3,8 +3,9 @@ use std::rc::Rc;
 use wasm_bindgen_futures::spawn_local;
 use web3::futures::StreamExt;
 use web3::transports::eip_1193::{Eip1193, Provider};
+use web3::types::Call;
 use yew::{
-    events::Event, html, Callback, Children, Component, Context, ContextProvider, Html, Properties,
+    events::Event, html, Callback, Children, Component, Context, ContextProvider, Html, Properties
 };
 use yew::{function_component, use_state};
 
@@ -41,28 +42,73 @@ pub struct EthereumProviderOld {
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct AccountState {
+    
     pub status: ConnectionStatus,
     pub web3: Web3Wrapper,
 }
 
+
+// type Callback = fn();
 impl AccountState {
+    
     pub async fn connect(&self) -> Result<(), web3::Error> {
+        wasm_logger::init(wasm_logger::Config::default());
+        log::info!("connect");
         self.web3.0.eth().request_accounts().await.map(|_| ())
+    }
+
+    pub async fn on<F>(&self, event_name: String, callback: F) 
+    where
+        F: Fn(String),
+    {
+        wasm_logger::init(wasm_logger::Config::default());
+        log::info!("chain changed");
+        let provider = Provider::default().unwrap().unwrap();
+        let transport: Eip1193 = Eip1193::new(provider);
+
+        
+        match event_name.clone() {
+            connected => {
+                let mut stream = transport.chain_changed_stream();
+
+                while let Some(chainid) = stream.next().await {
+                    callback(chainid.to_string());
+                }
+            },
+            _ => {
+                log::info!("no match");
+            },
+        }
+        
     }
 }
 
 #[function_component(EthereumProvider)]
 pub fn create(props: &Props) -> Html {
+    wasm_logger::init(wasm_logger::Config::default());
+    
     let provider = Provider::default().unwrap().unwrap();
     let transport: Eip1193 = Eip1193::new(provider);
+    // let ts = transport.clone();
     let web3 = Web3Wrapper(web3::Web3::new(transport));
 
+    // let mut stream = ts.chain_changed_stream();
+    
     let ctx = use_state(|| {
         Rc::new(AccountState {
             status: ConnectionStatus::default(),
             web3,
         })
     });
+
+    // spawn_local(async move {
+    //     while let Some(chainid) = stream.next().await {
+    //         log::info!("chain changed {:?}", &chainid);
+    //         // callback(chainid.to_string());
+    //     }
+    // });
+
+    
 
     html! {
         <ContextProvider<Rc<AccountState>> context={(*ctx).clone()}>
