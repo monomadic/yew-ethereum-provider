0cef3c4a87781a0188166ca3ae65bac7a7dc1c82
legendaryphoenixicarus <legendary.phoenix.icarus@gmail.com>
2022-06-13 05:32:27 +0300

    fix: add events

--- src/components/connect_account_button.rs
+++ src/components/connect_account_button.rs
@@ -8,30 +8,74 @@ use wasm_bindgen_futures::spawn_local;
 pub fn create() -> Html {
     wasm_logger::init(wasm_logger::Config::default());
 
-    let greeting = use_state(|| "No one has greeted me yet!".to_string());
+    let address = use_state(|| "Not Connected!".to_string());
+    let chain_id = use_state(|| "Not Connected".to_string() );
+    let connect_state = use_state(|| "Not Connected".to_string() );
+    let disconnect_state = use_state(|| "Not Connected".to_string() );
 
     let account = use_context::<Rc<AccountState>>().expect("No context found.");
 
-    let provider = account.clone();
+    let address_clone = (address).clone();
+    let chain_id_clone = (chain_id).clone();
+    let connect_state_clone = (connect_state).clone();
+    let disconnect_state_clone = (disconnect_state).clone();
 
-    {
-        let greeting = greeting.clone();
+    let connect = use_async(async move { 
+        let connect = account.connect().await;
+
+        let provider_for_chain = account.clone();
+        // chain_id.set(222);
         spawn_local(async move {
 
-            provider.on("connected".to_string(), move |chain_id| {
-                greeting.set(chain_id.to_string());
-            }).await;    
+            provider_for_chain.on("chainChanged".to_string(), move |chain| {
+                log::info!("chainChanged");
+                chain_id_clone.set(chain.to_string());
+            }).await;
         });
-    }
 
-    let connect = use_async(async move { account.connect().await });
+        let provider_for_accounts = account.clone();
+        spawn_local(async move {
+            
+            provider_for_accounts.on("accountsChanged".to_string(), move |account_address| {
+                log::info!("accountsChanged");
+                address_clone.set(account_address.to_string());
+            }).await;
+        });
+
+        let provider_for_connect = account.clone();
+        spawn_local(async move {
+            
+            provider_for_connect.on("connect".to_string(), move |account_address| {
+                connect_state_clone.set(account_address.to_string());
+            }).await;
+        });
+
+        let provider_for_disconnect = account.clone();
+        spawn_local(async move {
+            
+            provider_for_disconnect.on("disconnect".to_string(), move |account_address| {
+                disconnect_state_clone.set(account_address.to_string());
+            }).await;
+        });
+
+        // let chain_id = chain_id.clone();
+        
+
+        return connect;
+    });
     let onclick = Callback::from(move |_| connect.run());
 
     html! {
         <div>
             <button onclick={onclick}>{"Connect"}</button>
             <div>
-            <span>{"Chain Id: "}</span>{ (*greeting).to_string() }
+            <span>{"Chain Id: "}</span>{ (*chain_id).to_string() }
+            <br />
+            <span>{"Address : "}</span>{ (*address).to_string() }
+            <br />
+            <span>{"connect : "}</span>{ (*connect_state).to_string() }
+            <br />
+            <span>{"disconnect : "}</span>{ (*disconnect_state).to_string() }
             </div>
         </div>
     }
--- src/components/ethereum_provider.rs
+++ src/components/ethereum_provider.rs
@@ -45,24 +45,65 @@ impl AccountState {
     pub async fn on<F>(&self, event_name: String, callback: F) 
     where
         F: Fn(String),
-    {
-        let provider = Provider::default().unwrap().unwrap();
-        let transport: Eip1193 = Eip1193::new(provider);
-
+    {        
+        wasm_logger::init(wasm_logger::Config::default());
         
-        match event_name.clone() {
-            connected => {
-                let mut stream = transport.chain_changed_stream();
+        
+        if event_name == "accountsChanged" {
+            let provider = Provider::default().unwrap().unwrap();
+            let transport: Eip1193 = Eip1193::new(provider);
+            let mut stream = transport.accounts_changed_stream();
 
-                while let Some(chainid) = stream.next().await {
-                    callback(chainid.to_string());
-                }
-            },
-            _ => {
-            },
+            while let Some(accounts) = stream.next().await {
+                callback(accounts[0].to_string());
+            }
+        } else if event_name == "chainChanged" {
+            let provider = Provider::default().unwrap().unwrap();
+            let transport: Eip1193 = Eip1193::new(provider);
+            let mut stream = transport.chain_changed_stream();
+
+            while let Some(chainid) = stream.next().await {
+                callback(chainid.to_string());
+            }
+        } else if event_name == "connect" {
+            let provider = Provider::default().unwrap().unwrap();
+            let transport: Eip1193 = Eip1193::new(provider);
+            let mut stream = transport.connect_stream();
+
+            while let Some(chainid) = stream.next().await {
+                callback(chainid.unwrap().to_string());
+            }
+        } else if event_name == "disconnect" {
+            let provider = Provider::default().unwrap().unwrap();
+            let transport: Eip1193 = Eip1193::new(provider);
+            let mut stream = transport.disconnect_stream();
+
+            while let Some(error) = stream.next().await {
+                log::info!("disconnect {:?}", error);
+            }
         }
         
     }
+    
+    pub async fn on_chain_changed<F>(&self, callback: F) 
+    where
+        F: Fn(String),
+    {
+        self.on("chainChanged".to_string(), |chain_id|{
+            callback(chain_id.to_string());
+        }).await;
+    }
+
+    pub async fn on_account_changed<F>(&self, callback: F) 
+    where
+        F: Fn(String),
+    {
+        self.on("accountsChanged".to_string(), |address|{
+            callback(address.to_string());
+        }).await;
+    }
+
+    
 }
 
 #[function_component(EthereumProvider)]
