c0bd23b2f7152784b3429ced8d400b35d020bb80
monomadic <mono@madic>
2022-06-17 04:38:16 +0700

    events on provider

--- src/hooks/use_ethereum.rs
+++ src/hooks/use_ethereum.rs
@@ -8,25 +8,60 @@ use yew::prelude::*;
 #[derive(Clone)]
 pub struct UseEthereumHandle {
     provider: Provider,
-    inner: UseStateHandle<UseEthereumState>,
+    connected: UseStateHandle<bool>,
+    accounts: UseStateHandle<Option<Vec<H160>>>,
+    chain_id: UseStateHandle<Option<String>>,
 }
+
+// impl Default for UseEthereumHandle {
+//     fn default() -> Self {
+//         let inner = use_state(move || UseEthereumState::default());
+//         let connected = use_state(move || false);
+//
+//         Self {
+//             provider: Provider::default().unwrap().unwrap(),
+//             connected,
+//             addresses: Default::default(),
+//             chain_id: Default::default(),
+//         }
+//     }
+// }
 
 impl UseEthereumHandle {
     pub async fn connect(&self) {
         log::info!("connect()");
         let web3 = web3::Web3::new(Eip1193::new(self.provider.clone()));
         if let Ok(addresses) = web3.eth().request_accounts().await {
-            self.inner.set(UseEthereumState {
-                connected: true,
-                addresses: Some(addresses),
-            });
+            self.connected.set(true);
+            self.accounts.set(Some(addresses));
 
-            self.on_account_changed(move |addresses| {
+            self.on_accounts_changed(move |addresses| {
                 log::info!("event: accountsChanged");
-                self.inner.set(UseEthereumState {
-                    connected: true,
-                    addresses: Some(addresses),
-                });
+                if addresses.is_empty() {
+                    self.connected.set(false);
+                }
+                self.accounts.set(Some(addresses));
+            })
+            .await;
+
+            self.on_chain_changed(move |chain_id| {
+                log::info!("event: chainChanged: {}", chain_id);
+                self.chain_id.set(Some(chain_id));
+            })
+            .await;
+
+            self.on_connect(move |connect| {
+                log::info!("event: connect: {:?}", connect);
+                self.connected.set(true);
+            })
+            .await;
+
+            self.on_disconnect(move |chain_id| {
+                log::info!("event: disconnect: {}", chain_id);
+                // self.inner.set(UseEthereumState {
+                //     connected: true,
+                //     addresses: Some(addresses),
+                // });
             })
             .await;
         };
@@ -34,47 +69,72 @@ impl UseEthereumHandle {
 
     pub fn disconnect(&self) {
         log::info!("disconnect()");
-        self.inner.set(UseEthereumState {
-            connected: false,
-            addresses: None,
-        });
+        self.connected.set(false);
     }
 
     pub fn connected(&self) -> bool {
-        self.inner.connected
+        *self.connected
     }
 
     pub fn address(&self) -> Option<&H160> {
-        self.inner.addresses.as_ref().and_then(|a| a.first())
+        self.accounts.as_ref().and_then(|a| a.first())
     }
 
     pub fn display_address(&self) -> String {
         self.address().map(|a| a.to_string()).unwrap_or_default()
     }
 
-    pub async fn on_account_changed<F>(&self, callback: F)
+    pub async fn on_accounts_changed<F>(&self, callback: F)
     where
         F: Fn(Vec<web3::types::H160>),
     {
         let transport = Eip1193::new(self.provider.clone());
-        while let Some(chainid) = transport.accounts_changed_stream().next().await {
-            callback(chainid);
+        while let Some(accounts) = transport.accounts_changed_stream().next().await {
+            callback(accounts);
+        }
+    }
+
+    pub async fn on_chain_changed<F>(&self, callback: F)
+    where
+        F: Fn(String),
+    {
+        let transport = Eip1193::new(self.provider.clone());
+        while let Some(chainid) = transport.chain_changed_stream().next().await {
+            callback(chainid.to_string());
+        }
+    }
+
+    pub async fn on_connect<F>(&self, callback: F)
+    where
+        F: Fn(Option<String>),
+    {
+        let transport = Eip1193::new(self.provider.clone());
+        while let Some(connect) = transport.connect_stream().next().await {
+            callback(connect);
+        }
+    }
+
+    pub async fn on_disconnect<F>(&self, callback: F)
+    where
+        F: Fn(String),
+    {
+        let transport = Eip1193::new(self.provider.clone());
+        while let Some(err) = transport.disconnect_stream().next().await {
+            callback(err.to_string());
         }
     }
 }
 
-#[derive(Default, Clone)]
-pub struct UseEthereumState {
-    connected: bool,
-    addresses: Option<Vec<H160>>,
-}
-
 #[hook]
 pub fn use_ethereum(default: Option<Provider>) -> UseEthereumHandle {
-    let inner = use_state(move || UseEthereumState::default());
+    let connected = use_state(move || false);
+    let accounts = use_state(move || None as Option<Vec<H160>>);
+    let chain_id = use_state(move || None as Option<String>);
 
     UseEthereumHandle {
         provider: default.unwrap_or_else(|| Provider::default().unwrap().unwrap()),
-        inner,
+        connected,
+        accounts,
+        chain_id,
     }
 }
